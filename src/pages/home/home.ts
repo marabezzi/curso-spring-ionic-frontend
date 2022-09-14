7488287819970abfed7a7a559fb9b00177030d8a
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-09-14 11:33:34 -0300

    Salvando os dados do usuário logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,7 +24,7 @@ export class HomePage {
   public login(){
     this.auth.authenticate(this.creds)
     .subscribe(Response => {
-      console.log(Response.headers.get('Authorization'));
+      this.auth.successfulLogin(Response.headers.get('Authorization'));
       this.navCtrl.setRoot('CategoriasPage');
     },
     error => {});
